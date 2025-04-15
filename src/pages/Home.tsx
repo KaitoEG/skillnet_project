9033c0b6eb0836417c2c5b59edf7eeb487fdac11
KaitoEG <eaghtspr@gmail.com>
2025-04-15 01:25:23 -0400

    body

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,21 +2,22 @@
 import Header from '../components/organism/Header';
 import Footer2 from '../components/organism/Footer2';
 import Footer from '../components/organism/Footer';
-
+import HeroImage from '../components/molecules/HeroImage';
+import AboutSkillNet from '../components/molecules/AboutSkillNet';
+import Objective from '../components/molecules/Objective';
+import FeaturesSkillNet from '../components/molecules/FeaturesSkillNet';
 
 function Home() {
   return (
     <div>
       <Header />
 
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>Tengo sueñoooo ....zzzz.....zzz......zz..z.....</h1>
-      </div>
+      <HeroImage />
+      <AboutSkillNet />
+      <Objective />
+      <FeaturesSkillNet />
 
-      {/* Aquí insertamos Footer2 */}
       <Footer2 />
-
-      {/* Y luego tu footer “principal” */}
       <Footer />
     </div>
   );
